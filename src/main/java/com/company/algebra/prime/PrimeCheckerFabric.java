15d4178f57a3c74b5ba6af3e773636a83f605877
тимофей <stars6392@gmail.com>
2023-04-10 21:19:06 +0300

    add some changes

--- src/main/java/com/company/algebra/prime/PrimeCheckerFabric.java
+++ src/main/java/com/company/algebra/prime/PrimeCheckerFabric.java
@@ -8,13 +8,17 @@ import com.company.algebra.residue.impl.RSAMath;
 import java.util.Map;
 
 public final class PrimeCheckerFabric {
-    private static Map<PrimeCheckerType, PrimeChecker> typeAndInstance = Map.of(
+    private static final Map<PrimeCheckerType, PrimeChecker> typeAndInstance = Map.of(
             PrimeCheckerType.FERMAT, new FermatPrimeChecker(),
             PrimeCheckerType.MILLER_RABIN, new MillerRabinPrimeChecker(),
             PrimeCheckerType.SOLOVEY_STRASSEN, new SolovayStrassenPrimeChecker(new RSAMath())
     );
-    
+
     public static PrimeChecker getInstance(PrimeCheckerType type) {
         return typeAndInstance.get(type).clone();
+    }
+
+    private PrimeCheckerFabric() {
+
     }
 }
